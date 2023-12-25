b74b3082c667f692a37e488b80c2b8ddf12bac85
VansRouges <evansagina57@gmail.com>
2023-12-25 03:32:59 +0100

    data pulled successfully

---
+++ app/action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { data } from "./_data";
+
+export const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
+};
