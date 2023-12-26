690ed5f1831d08bf6c7a5beb8fd9e9d9d4cf81a3
VansRouges <evansagina57@gmail.com>
2023-12-26 12:40:26 +0100

    animation added&data now pulled from server-action

--- app/action.tsx
+++ app/action.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { data } from "./_data";
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
@@ -9,7 +10,7 @@ export const fetchAnime = async (page: number) => {
 
   const data = await response.json();
 
-  console.log(data);
-
-  return data;
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
 };
